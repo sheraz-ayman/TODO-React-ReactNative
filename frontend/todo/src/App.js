804e31b720319eaeeb17b947f7481733abf59787
sheraz-ayman <sheraz.ayman@gmail.com>
2023-10-12 14:07:38 +0300

    Login api call

--- frontend/todo/src/App.js
+++ frontend/todo/src/App.js
@@ -4,15 +4,20 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Header from './components/partial/Header';
 import Home from './pages/Home/Home';
+import { useState } from 'react';
 
 function App() {
+
+  const info = localStorage.getItem('user');
+  const[user,setUser] = useState(JSON.parse(info))
+
   return (
     <>
     <BrowserRouter>
     <Header/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/login' element={<Login user={user} setUser={setUser}/>}></Route>
       <Route path='/register' element={<Register/>}></Route>
     </Routes>
     </BrowserRouter>
--- frontend/todo/src/pages/Login/Login.js
+++ frontend/todo/src/pages/Login/Login.js
@@ -1,13 +1,108 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React,{useEffect, useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../../services/api';
+import {ToastContainer, toast} from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
 
-function Login() {
+function Login({user,setUser}) {
+  const navigation=useNavigate()
+
+  const [form,setForm]=useState({
+    username:'',
+    password:''
+  });
+
+  useEffect(()=>{
+    if(user){
+      navigation('/')
+    }
+  },[])
+
+  const [errors,setErrors]=useState(null)
+
+  const handleChange = (e) => {
+    setForm({ ...form,[e.target.name]:e.target.value})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const result = await login(form);
+  
+    console.log("form", result);
+    setErrors(null);
+  
+    if (result.status === 200) {
+      if (result.data.status === 200) {
+        localStorage.setItem('user', JSON.stringify(result.data.data));
+        navigation('/');
+        return;
+      }
+    }
+    if (result.data.status === 201) {
+      setErrors(result.data.data);
+      return;
+    }
+    if (result.data.status === 202) {
+      toast(result.data.message);
+      return;
+    }
+  };
+  
   return (
-    <div>
-        <h1>Login</h1>
-        <p>don't have an acoount <Link to="/register">register</Link> now!</p>
+    <div className="container">
+      <ToastContainer/>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="card-title text-center">Login</h2>
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Email address or Username
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    onChange={handleChange}
+                    name='username'
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                  {errors?.username &&(
+                    <div id="emailHelp" className="form-text">
+                    {errors.username.msg}
+                  </div>
+                  )}
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    onChange={handleChange}
+                    name='password'
+                  />
+                   {errors?.password &&(
+                    <div id="emailHelp" className="form-text">
+                    {errors.password.msg}
+                  </div>
+                  )}
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
