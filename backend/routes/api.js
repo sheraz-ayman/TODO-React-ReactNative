9ce491156f2a792d237a7fba40c00e173b54813e
sheraz-ayman <sheraz.ayman@gmail.com>
2023-10-12 04:06:09 +0300

    login

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,9 +1,12 @@
 import express from "express"
 import Register from "../controllers/Register.controller.js";
 import { RegisterSchema } from "../validationSchema/RegisterSchema.js";
+import Login from "../controllers/Login.controller.js";
+import { LoginSchema } from "../validationSchema/LoginSchema.js";
 
 const apiRoute=express.Router();
 
 apiRoute.post('/register' ,RegisterSchema, Register)
+apiRoute.post('/login' ,LoginSchema, Login)
 
 export default apiRoute
