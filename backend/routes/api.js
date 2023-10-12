588d0022f8befc6f17174934db3809e0f274ba33
sheraz-ayman <sheraz.ayman@gmail.com>
2023-10-12 05:30:42 +0300

    get all todos

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -5,16 +5,21 @@ import { RegisterSchema } from "../validationSchema/RegisterSchema.js";
 import Login from "../controllers/Login.controller.js";
 import { LoginSchema } from "../validationSchema/LoginSchema.js";
 import { createTodo } from "../controllers/Todo.controller.js";
+import { GetTodos } from "../controllers/TodoList.controllers.js";
 
-const apiRoute=express.Router();
-export const apiProtected=express.Router();
+const apiRoute = express.Router();
+export const apiProtected = express.Router();
 
 
-apiRoute.post('/register' ,RegisterSchema, Register)
-apiRoute.post('/login' ,LoginSchema, Login)
+apiRoute.post('/register', RegisterSchema, Register)
+apiRoute.post('/login', LoginSchema, Login)
 
 //protected routes;
-apiProtected.post("/createTodo",[check("desc" , "Todo desc is required").exists()],
-createTodo
+apiProtected.post("/createTodo", [check("desc", "Todo desc is required").exists()],
+    createTodo
+)
+
+apiProtected.get("/todolist",
+    GetTodos
 )
 export default apiRoute
